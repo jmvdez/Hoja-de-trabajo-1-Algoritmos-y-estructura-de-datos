9972ab869d34139b47d296c6fc25a221bb31a85c
Kenneth Maldonado <maldonadovaldez900@gmail.com>
2026-06-02 01:17:31 -0600

    Agregar pruebas de la clase Cuenta

---
+++ src/banco/CuentaTest.java
@@ -0,0 +1,16 @@
+package banco;
+
+public class CuentaTest {
+
+    public void pruebaCrearCuenta() {
+        System.out.println("Prueba crear cuenta");
+    }
+
+    public void pruebaDepositar() {
+        System.out.println("Prueba depositar");
+    }
+
+    public void pruebaRetirar() {
+        System.out.println("Prueba retirar");
+    }
+}
