c557b1bf515e07b5a88398749300a08f5bae9577
Kenneth Maldonado <maldonadovaldez900@gmail.com>
2026-06-02 01:01:38 -0600

    Agregar pruebas de funcionamiento en Main

--- src/banco/Main.java
+++ src/banco/Main.java
@@ -1,7 +1,22 @@
 package banco;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Sistema Bancario");
+
+        Banco banco = new Banco();
+
+        banco.abrirCuenta();
+
+        Cuenta cuenta = new Cuenta(
+                "001",
+                "Kenneth");
+
+        cuenta.depositar(100);
+
+        cuenta.retirar(50);
+
+        cuenta.mostrarSaldo();
+
     }
 }
